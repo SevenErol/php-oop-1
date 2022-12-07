eaa37d271a7e39ee4304c1e22eb0fafd78ff4883
SevenErol <lorenzo.maggi1998@gmail.com>
2022-12-07 15:50:55 +0100

    bonus 1 added more genres to a single movie

--- index.php
+++ index.php
@@ -1,16 +1,31 @@
 <?php
 
+class Genres
+{
+    public $genreOne;
+    public $genreTwo;
+    public $genreThree;
+
+    function __construct($genreOne, $genreTwo, $genreThree)
+    {
+        $this->genreOne = $genreOne;
+        $this->genreTwo = $genreTwo;
+        $this->genreThree = $genreThree;
+    }
+}
+
 class Movie
 {
     public $title;
     public $description;
     public $link;
 
-    function __construct($title, $description, $link)
+    function __construct($title, $description, $link, $genres)
     {
         $this->title = $title;
         $this->description = $description;
         $this->link = $link;
+        $this->genres = $genres;
     }
 
     function R_Rated_movie($age)
@@ -26,12 +41,12 @@ class Movie
     }
 }
 
-$matrix = new Movie('Matrix', 'bla bla bla', 'https://lorem');
+$matrix = new Movie('Matrix', 'bla bla bla', 'https://lorem', new Genres('Fantascienza', 'Azione', 'Avventura'));
 
 $matrix->R_Rated_movie(18);
 
 var_dump($matrix);
 
-$aboutTime = new Movie('About Time', 'bla bla bla', 'https://ipsum');
+$aboutTime = new Movie('About Time', 'bla bla bla', 'https://ipsum', new Genres('Romantico', 'Commedia', null));
 
 var_dump($aboutTime);
